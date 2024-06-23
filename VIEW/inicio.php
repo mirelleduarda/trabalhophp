d315119429944b8cd592491666113ff378956555
Mirelle Eduarda R. Oliveira <93480351+mirelleduarda@users.noreply.github.com>
2024-06-22 23:19:51 -0300

    new file:   VIEW/inicio.php 	modified:   VIEW/menu.php 	modified:   css/style.css

--- VIEW/inicio.php
+++ VIEW/inicio.php
@@ -1,5 +1,3 @@
- 
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -34,7 +32,7 @@
                 </div>
             </a>
             <ul id="nav-mobile" class="left hide-on-med-and-down">
-                <li><a href="/trabalhophp/VIEW/inicio.php">Menu</a></li>
+                <li><a href="/trabalhophp/VIEW/menu.php">Menu</a></li>
                 <li><a href="/trabalhophp/VIEW/Categoria/lstCategoria.php">Categorias</a></li>
                 <li><a href="badges.html">Cardápio</a></li>
                 <li><a href="collapsible.html">Funcionários</a></li>
@@ -50,8 +48,37 @@
             <div class="espacocafe2">
                 <img class="espacocafe2" src="/trabalhophp/images/espacocafe2.png">
             </div>
+            <table>
+                <p class="apresentacao__conteudo__texto">
+                    Bem-vindo ao nosso refúgio aromático, onde cada
+                    xícara conta uma história de paixão e precisão.
+                    Em nossa cafeteria, não servimos apenas café; criamos
+                    uma experiência sensorial que transcende o simples ato
+                    de tomar uma bebida. Cada grão é escolhido com cuidado,
+                    cada torra é uma sinfonia de sabores, e cada barista é um
+                    artista, dedicado a transformar sua visita em um momento inesquecível.
+                </p>
+                
+                <p class="apresentacao__conteudo__texto">
+                    Nossa jornada começa nas plantações distantes, onde os melhores
+                    grãos de café são colhidos à mão, com respeito pela terra e pelas
+                    mãos que cultivam essa preciosidade. No coração de nossa operação
+                    está o compromisso com a sustentabilidade e a qualidade, garantindo
+                    que cada xícara seja uma celebração do trabalho árduo dos produtores
+                    e da magia que o café carrega.
+                </p>
+
+                <p class="apresentacao__conteudo__texto">
+                    Ao entrar em nossa cafeteria, você é recebido por um ambiente acolhedor,
+                    onde o aroma do café recém-moído dança no ar. Nossos baristas apaixonados
+                    estão prontos para guiá-lo por uma jornada de sabores, oferecendo não apenas
+                    bebidas exclusivas, mas também uma narrativa que conecta cada cliente à riqueza
+                    do café. Sinta-se em casa, relaxe e permita que cada gole o transporte para um
+                    universo de aromas e sensações, onde a simplicidade de uma xícara de café se torna
+                    uma experiência única e específica.
+                </p>
+            </table>    
         </div>
-        
     </section>
     <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="/view/js/materialize.js"></script>
